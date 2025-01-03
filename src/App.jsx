ca717d886fb6c139b0c8c82684125a64332bd45d
rohanmahdik <93382658+RohanMahadik@users.noreply.github.com>
2025-01-03 21:06:16 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dummy Application</h1>
+      <h1>Dummy Application rohan mahadik</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
